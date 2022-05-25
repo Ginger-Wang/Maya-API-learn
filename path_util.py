472d2a9a7d5473736f400833b3c40ecb4cb023de
Ginger-Wang <56863322+Ginger-Wang@users.noreply.github.com>
2022-05-25 21:11:57 +0800

    Create path_util.py

---
+++ path_util.py
@@ -0,0 +1,26 @@
+import os
+
+UE_PROJECT_ROOT = "../../../Cobra"
+UE_CONTENT_PYTHON = "content/python/"
+UE_TOOL_PYTHON = "tools/python/unrealscripts/"
+
+
+def ue_project_root():
+    return os.path.realpath(os.path.join(os.getcwd(), UE_PROJECT_ROOT))
+
+
+def ue_content_python_path():
+    return os.path.join(ue_project_root(), UE_CONTENT_PYTHON)
+
+
+def ue_tool_python_path():
+    return os.path.join(ue_project_root(), UE_TOOL_PYTHON)
+
+
+def qt_icon_path():
+    return os.path.join(ue_tool_python_path(), 'QtUtil/Icons/')
+
+
+def current_working_directory():
+    return os.getcwd()
+
